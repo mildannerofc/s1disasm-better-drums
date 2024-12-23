95d1229752acbb551552ceb6f48dc63b18475b0c
Mildanner <cianocriativo408@gmail.com>
2024-12-22 21:12:56 -0300

    Update SampleTable.asm

--- SampleTable.asm
+++ SampleTable.asm
@@ -2,23 +2,29 @@
 ; ---------------------------------------------------------------
 SampleTable:
 	;			type			pointer		Hz
-	dcSample	TYPE_DPCM, 		Kick, 		8000				; $81
-	dcSample	TYPE_PCM,		Snare,		24000				; $82
-	dcSample	TYPE_DPCM, 		Timpani, 	7250				; $83
-	dcSample	TYPE_NONE										; $84
-	dcSample	TYPE_NONE										; $85
-	dcSample	TYPE_NONE										; $86
-	dcSample	TYPE_NONE										; $87
-	dcSample	TYPE_DPCM, 		Timpani, 	9750				; $88
-	dcSample	TYPE_DPCM, 		Timpani, 	8750				; $89
-	dcSample	TYPE_DPCM, 		Timpani, 	7150				; $8A
-	dcSample	TYPE_DPCM, 		Timpani, 	7000				; $8B
+	dcSample	TYPE_PCM, 		Kick, 		25100				; $81
+	dcSample	TYPE_PCM,		Snare,		25100				; $82
+	dcSample	TYPE_PCM, 		Timpani, 	22050				; $83
+	dcSample	TYPE_PCM,		Clap,		22050				; $84
+	dcSample	TYPE_PCM,		Cymbal,		22050				; $85
+	dcSample	TYPE_PCM,		Scratch,	16000				; $86
+	dcSample	TYPE_PCM,		OrchHit, 	16000				; $87
+	dcSample	TYPE_PCM, 		Timpani, 	22650				; $88
+	dcSample	TYPE_PCM, 		Timpani, 	20600				; $89
+	dcSample	TYPE_PCM, 		Timpani, 	20100				; $8A
+	dcSample	TYPE_PCM, 		Timpani, 	19900				; $8B
 	dcSample	TYPE_PCM,		SegaPCM,	0, FLAGS_SFX		; $8C	NOTE: sample rate is auto-detected from WAV file
+	dcSample	TYPE_PCM, 		Alright, 	10655				; $8D
 	dc.w	-1	; end marker
 
 ; ---------------------------------------------------------------
-	incdac	Kick, "sound/dac/kick.dpcm"
-	incdac	Snare, "sound/dac/snare.pcm"
-	incdac	Timpani, "sound/dac/timpani.dpcm"
+	incdac	Kick, "sound/dac/hqkick.wav"
+	incdac	Snare, "sound/dac/hqsnare.wav"
+	incdac	Timpani, "sound/dac/hqtimpani.wav"
+	incdac	Clap, "sound/dac/hqclap.wav"
+	incdac	Cymbal, "sound/dac/hqcymbal.wav"
+	incdac	Scratch, "sound/dac/scratch.wav"
+	incdac	OrchHit, "sound/dac/orchestrahit.wav"
+	incdac	Alright, "sound/dac/alright.wav"
 	incdac	SegaPCM, "sound/dac/sega.wav"
 	even
